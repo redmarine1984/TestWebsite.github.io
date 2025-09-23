b62c12fc90f68f3716d20099ed950a9f5ac1d202
redmarine1984 <56165644+redmarine1984@users.noreply.github.com>
2025-09-23 16:08:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const products = [
-    { id: 1, name: "iPhone 15 Otter Box", price: 29.99, img: "https://i5.walmartimages.com/seo/OtterBox-Defender-Series-Pro-Case-for-Apple-iPhone-15-Plus-and-iPhone-14-Plus-Black_d947839e-28a3-4f42-a55e-5a296fb70d8f.d71b95f43f3874f3b102361dee39ccfd.jpeg?odnHeight=573&odnWidth=573&odnBg=FFFFFF"},
-    { id: 2, name: "Dell Inspiron 3000 Laptop", price: 499.99, img: "https://i5.walmartimages.com/seo/Dell-Inspiron-3000-Laptop-15-6-Non-touch-Intel-Celeron-Processor-N4020-Graphics-600-4GB-DDR4-Memory-128GB-SSD-Hard-Drive-Windows-10-Home-S-mode_b106bcee-6f1f-4c18-87fc-b1f9677d39df.65d263e5ab6bcaa6d9edae94627ece7f.jpeg"},
-    { id: 3, name: "iPhone 16 256Gb (Refurbished)", price: 399.99, img: "https://m.media-amazon.com/images/I/71QkWOSDkmL.jpg"},
-    { id: 4, name: "iPhone 15 256Gb (Refurbished)", price: 299.99, img: "https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPhone-15-Pro-Max-Black-Titanium-frontimage"},
-    { id: 5, name: "Samsung Galaxy 21 (Refurbished)", price: 299.99, img: "https://m.media-amazon.com/images/I/616kK0b+d+L._UF894,1000_QL80_.jpg"},
-    { id: 6, name: "Moto G Play (Refurbished)", price: 99.99, img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6568/6568429_sd.jpg;maxHeight=1920;maxWidth=900?format=webp"},
-    { id: 7, name: "Moto Razr (Refurbished)", price: 299.99, img: "https://m.media-amazon.com/images/I/51Q+JcbYziL.jpg"},
-    { id: 8, name: "Moto G Otter Box", price: 29.99, img: "https://www.otterbox.com/on/demandware.static/-/Sites-masterCatalog/default/dw6333c29e/productimages/dis/cases-screen-protection/commuter-moto-g-stylus-5g-2023/commuter-moto-g-stylus-5g-2023-black-2.jpg"},
-    { id: 9, name: "iPhone 15 Otter Box", price: 39.99, img: "https://m.media-amazon.com/images/I/71dTkLoBenL.jpg"},
-    { id: 10, name: "Acer Aspire 1 A115-31-C2Y3 Laptop", price: 299.99, img: "https://i5.walmartimages.com/asr/fc604dd8-8761-486d-80ff-59551a330dd5.2a4dd8b264365b821849532a3d7b3d73.jpeg"}
+    { id: 1, name: "iPhone 15 Otter Box ", price: 29.99, img: "https://i5.walmartimages.com/seo/OtterBox-Defender-Series-Pro-Case-for-Apple-iPhone-15-Plus-and-iPhone-14-Plus-Black_d947839e-28a3-4f42-a55e-5a296fb70d8f.d71b95f43f3874f3b102361dee39ccfd.jpeg?odnHeight=573&odnWidth=573&odnBg=FFFFFF"},
+    { id: 2, name: "Dell Inspiron 3000 Laptop ", price: 499.99, img: "https://i5.walmartimages.com/seo/Dell-Inspiron-3000-Laptop-15-6-Non-touch-Intel-Celeron-Processor-N4020-Graphics-600-4GB-DDR4-Memory-128GB-SSD-Hard-Drive-Windows-10-Home-S-mode_b106bcee-6f1f-4c18-87fc-b1f9677d39df.65d263e5ab6bcaa6d9edae94627ece7f.jpeg"},
+    { id: 3, name: "iPhone 16 256Gb (Refurbished) ", price: 399.99, img: "https://m.media-amazon.com/images/I/71QkWOSDkmL.jpg"},
+    { id: 4, name: "iPhone 15 256Gb (Refurbished) ", price: 299.99, img: "https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPhone-15-Pro-Max-Black-Titanium-frontimage"},
+    { id: 5, name: "Samsung Galaxy 21 (Refurbished) ", price: 299.99, img: "https://m.media-amazon.com/images/I/616kK0b+d+L._UF894,1000_QL80_.jpg"},
+    { id: 6, name: "Moto G Play (Refurbished) ", price: 99.99, img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6568/6568429_sd.jpg;maxHeight=1920;maxWidth=900?format=webp"},
+    { id: 7, name: "Moto Razr (Refurbished) ", price: 299.99, img: "https://m.media-amazon.com/images/I/51Q+JcbYziL.jpg"},
+    { id: 8, name: "Moto G Otter Box ", price: 29.99, img: "https://www.otterbox.com/on/demandware.static/-/Sites-masterCatalog/default/dw6333c29e/productimages/dis/cases-screen-protection/commuter-moto-g-stylus-5g-2023/commuter-moto-g-stylus-5g-2023-black-2.jpg"},
+    { id: 9, name: "iPhone 15 Otter Box ", price: 39.99, img: "https://m.media-amazon.com/images/I/71dTkLoBenL.jpg"},
+    { id: 10, name: "Acer Aspire 1 A115-31-C2Y3 Laptop ", price: 299.99, img: "https://i5.walmartimages.com/asr/fc604dd8-8761-486d-80ff-59551a330dd5.2a4dd8b264365b821849532a3d7b3d73.jpeg"}
 ];
 
 let cart = [];
