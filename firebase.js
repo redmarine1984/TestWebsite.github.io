4208075c7fbdbab867d5e5206f964ee40db88a7d
redmarine1984 <56165644+redmarine1984@users.noreply.github.com>
2025-09-29 19:14:14 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,13 @@
 // Replace with your own Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCgu4CYsYkUoVDH3w5AHGOuiEQN2yhmHSo",
+  authDomain: "celltherapy-69b1d.firebaseapp.com",
+  databaseURL: "https://celltherapy-69b1d-default-rtdb.firebaseio.com",
+  projectId: "celltherapy-69b1d",
+  storageBucket: "celltherapy-69b1d.firebasestorage.app",
+  messagingSenderId: "714260651727",
+  appId: "1:714260651727:web:1e1e8b6416f31a1a7121ba",
+  measurementId: "G-QF5QM6YLJP"
 };
 
 // Initialize Firebase
